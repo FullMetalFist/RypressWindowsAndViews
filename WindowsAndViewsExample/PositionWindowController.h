3670f56df4747ce07d00b2b56415c6236ceca5aa
Michael Vilabrera <m.vilabrera@gmail.com>
2018-07-16 04:14:56 -0400

    add PositionWindow xib & PositionWindowController files

---
+++ WindowsAndViewsExample/PositionWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PositionWindowController.h
+//  WindowsAndViewsExample
+//
+//  Created by Michael Vilabrera on 7/15/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PositionWindowController : NSWindowController
+
+@end
---
+++ WindowsAndViewsExample/PositionWindowController.m
@@ -0,0 +1,23 @@
+//
+//  PositionWindowController.m
+//  WindowsAndViewsExample
+//
+//  Created by Michael Vilabrera on 7/15/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+#import "PositionWindowController.h"
+
+@interface PositionWindowController ()
+
+@end
+
+@implementation PositionWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
